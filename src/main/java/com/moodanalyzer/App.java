b1722d563c678cd8496fd79c76107b8ed403f383
Rajshri Wagh <mahajan.rajshri16@gmail.com>
2022-12-31 17:30:52 +0530

    Add: Initial Commit

---
+++ src/main/java/com/moodanalyzer/App.java
@@ -0,0 +1,13 @@
+package com.moodanalyzer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Moodanalyzer!" );
+    }
+}
