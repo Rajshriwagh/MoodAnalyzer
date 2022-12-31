0d2ad4b893fa3499e79d733fa1bae2eb31b7c55a
Rajshri Wagh <mahajan.rajshri16@gmail.com>
2022-12-31 18:11:53 +0530

    Add: Throwing custom exceptions

--- src/main/java/com/moodanalyzer/MoodAnalyzer.java
+++ src/main/java/com/moodanalyzer/MoodAnalyzer.java
@@ -1,19 +1,28 @@
 package com.moodanalyzer;
+import com.moodanalyzer.MoodAnalysisException;
 
 public class MoodAnalyzer {
 	private String message;
+
     public MoodAnalyzer(String message) {
         this.message = message;
     }
 
-    public String analyseMood() {
-    	try {
+    public String analyseMood(String message) throws MoodAnalysisException {
+        this.message = message;
+        return analyseMood();
+    }
+
+    public String analyseMood() throws MoodAnalysisException {
+        try {
+            if (message.length() == 0)
+                throw new MoodAnalysisException(MoodAnalysisException.exceptionType.ENTERED_EMPTY, "Please enter Proper Mood");
             if (message.contains("Sad"))
                 return "SAD";
             else
                 return "HAPPY";
         } catch (NullPointerException e) {
-            return "HAPPY";
+            throw new MoodAnalysisException(MoodAnalysisException.exceptionType.ENTERED_NULL, "Please enter Proper Mood");
         }
     }
 }
--- src/test/java/com/moodanalyzer/MoodAnalyzerTest.java
+++ src/test/java/com/moodanalyzer/MoodAnalyzerTest.java
@@ -1,27 +1,66 @@
 package com.moodanalyzer;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import com.moodanalyzer.MoodAnalysisException;
 
 public class MoodAnalyzerTest {
-	 @Test
-	    public void givenMessage_WhenSad_ShouldReturn_Sad() {
-	        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a Sad Message");
-	        String mood = moodAnalyzer.analyseMood();
-	        Assert.assertEquals("SAD",mood);
+	@Test
+    public void givenMessgae_WhenSad_ShouldReturn_Sad() throws MoodAnalysisException {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Sad Mood");
+        String mood;
+        mood = moodAnalyzer.analyseMood();
+        Assert.assertEquals("SAD", mood);
+    }
 
-	    }
+    /*
+     *This Test Case Excepts
+     * Happy Mood
+     */
+    @Test
+    public void givenMessage_WhenNotSad_ShouldReturn_Happy() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in any Mood");
+        String mood;
+        try {
+            mood = moodAnalyzer.analyseMood();
+            Assert.assertEquals("HAPPY", mood);
+        }
+        catch (MoodAnalysisException e){
+            System.out.println(e);
+        }
 
-	    @Test
-	    public void givenMessage_WhenNotSad_ShouldReturn_Happy() {
-	        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is Happy Message");
-	        String mood = moodAnalyzer.analyseMood();
-	        Assert.assertEquals("HAPPY",mood);
-	    }
-	    
-	    @Test
-	    public void givenNullMood_ShouldReturn_Happy() {
-	        MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
-	        String mood = moodAnalyzer.analyseMood();
-	        Assert.assertEquals("HAPPY",mood);
-	    }
+    }
+
+    /*
+     *This Test Case Will Check For
+     * Null Pointer Exception
+     */
+    @Test
+    public void givenNullMood_ShouldReturn_Happy() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
+        String mood;
+        try {
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(MoodAnalysisException.class);
+            mood = moodAnalyzer.analyseMood();
+            Assert.assertEquals("HAPPY", mood);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+     *This Test Case Will Check For
+     * Null Pointer Exception
+     * for empty and null values
+     */
+    @Test
+    public void givenNullMood_ShouldThrow_Exception() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
+        try {
+            moodAnalyzer.analyseMood(null);
+        } catch (MoodAnalysisException e) {
+            Assert.assertEquals(MoodAnalysisException.exceptionType.ENTERED_NULL, e.type);
+        }
+    }
 }
